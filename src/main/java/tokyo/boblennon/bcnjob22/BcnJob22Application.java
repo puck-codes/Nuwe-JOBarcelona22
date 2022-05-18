7d81d74ec61d2fa4e667448836d7cb16f33404fb
Bob-Lennon <brayansc1119@gmail.com>
2022-05-18 14:40:57 +0200

    Initial commit

---
+++ src/main/java/tokyo/boblennon/bcnjob22/BcnJob22Application.java
@@ -0,0 +1,13 @@
+package tokyo.boblennon.bcnjob22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcnJob22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcnJob22Application.class, args);
+	}
+
+}
